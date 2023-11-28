c564b293aa780a56e169323be5f3754ba339ec02
yamaneki <c0b22155ef@edu.teu.ac.jp>
2023-11-28 14:08:54 +0900

    練習１：爆弾Surfaceの追加~

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -11,15 +12,24 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img = pg.Surface((20, 20))   #練習１:透明のSurfaceを作る
+    bb_img.set_colorkey((0, 0, 0))  #練習１：黒い部分を透明に
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #練習１:赤い半径10の円を書く
+    bb_rct = bb_img.get_rect()  #練習２：爆弾SurfaceのRentを抽出する
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    screen.blit(bb_img, bb_rct)
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:  #×ボタンを押すと..
                 return
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
